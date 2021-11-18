7a47ed268cd1fe01d3c833ff64a5a23d5f6970e6
Kishor <kishor1124@gmail.com>
2021-11-18 19:07:36 +0530

    angular inbuilt pipes

--- simple-app/src/app/app.module.ts
+++ simple-app/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
 import { StudentComponent } from './student/student.component';
+
 @NgModule({
   declarations: [
     AppComponent, UserComponent, StudentComponent
---
+++ simple-app/src/app/employee.ts
@@ -0,0 +1,10 @@
+export class Employee { 
+    constructor(
+        public id : number, 
+        public name : string, 
+        public gender : string, 
+        public dob : Date, 
+        public salary : number){
+            
+        }
+}
